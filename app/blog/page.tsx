25217a91669044fa861b87dc1a38f0c59325b00b
Philani Ncube <ncbphi001@gmail.com>
2023-06-16 18:26:27 +0200

    add privacy policy and blog stub

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+import Container from "@/components/layout/Container";
+
+const page = () => {
+  return (
+    <Container>
+      <h1 className="text-3xl md:text-4xl font-medium">Blog</h1>
+    </Container>
+  );
+};
+export default page;
---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,8 @@
+import Container from "@/components/layout/Container";
+
+const page = () => {
+  return <Container>
+    <h1 className="text-3xl md:text-4xl font-medium">Privacy Policy</h1>
+  </Container>;
+};
+export default page;
