61ac4efc8d5821aba157aaf1935f07cd60f2c01e
Philani Ncube <ncbphi001@gmail.com>
2023-06-16 17:56:53 +0200

    success page

--- app/checkout/success/page.tsx
+++ app/checkout/success/page.tsx
@@ -1,3 +1,9 @@
+import Container from "@/components/layout/Container";
+import { Separator } from "@/components/ui/separator";
+import { getOrderById } from "@/lib/fetchers/orders";
+import { formatPrice } from "@/lib/utils";
+import Image from "next/image";
+
 const page = async ({
   searchParams,
 }: {
@@ -5,6 +11,8 @@ const page = async ({
 }) => {
 
   const { order_id } = searchParams;
+
+  const order = await getOrderById(order_id!);
 
   // const query = await fetch(
   //   `https://sandbox.payfast.co.za/​eng/query/validate`, {
@@ -18,6 +26,46 @@ const page = async ({
 
   // console.log(query);
 
-  return <div>Success Page</div>;
+  return (
+    <main>
+      <Container>
+        <h1 className="text-3xl md:text-4xl">
+          Thank You for your order {order.first_name}.
+        </h1>
+        <p className="text-md max-w-[50ch]">
+          Your order number is being printed and we aim to get it delivered in
+          the next 3 to 6 working days. We will keep you posted.
+        </p>
+
+        <Separator className="my-4" />
+
+        <div className="grid grid-cols-1 max-w-[700px]">
+          {order.order_items.map((item) => (
+            <div
+              key={item.variantSKU}
+              className="w-full flex space-x-3 border-b"
+            >
+              <Image
+                src={item.image}
+                width={400}
+                height={400}
+                className="w-[200px] object-cover aspect-square"
+                alt={item.size}
+              />
+              <div className="mt-2 w-full px-2">
+                <h2 className="text-xl font-semibold">{item.productTitle}</h2>
+                <h2 className="text-md font-semibold text-slate-400">
+                  {item.size}
+                </h2>
+                <h2 className="text-lg font-semibold text-slate-400">
+                  {formatPrice(item.price)} x {item.qty}
+                </h2>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Container>
+    </main>
+  );
 };
 export default page;
