a619cf244d72dbaaf792987a0845f0f8299890bb
Philani Ncube <ncbphi001@gmail.com>
2023-06-16 14:32:40 +0200

    update notify api route

--- app/api/notify/route.ts
+++ app/api/notify/route.ts
@@ -11,9 +11,20 @@ export async function POST(request: Request) {
 
   console.log('Notify')
 
+const SupabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const SupabaseSecret = process.env.SUPABASE_SECRET
+
+if(!SupabaseUrl || !SupabaseSecret || typeof SupabaseUrl === 'undefined' || typeof SupabaseSecret === 'undefined' || typeof  SupabaseUrl !== 'string' || typeof SupabaseSecret !== 'string' || typeof SupabaseUrl === 'undefined' || typeof SupabaseSecret === 'undefined') {
+  console.log('Error', 'Supabase Url or Secret is missing')
+  return NextResponse.json({
+    status: 200,
+    body: 'Supabase Url or Secret is missing'
+  })
+}
+
 const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SECRET!
+  SupabaseUrl,
+  SupabaseSecret
 )
   const data = await request.formData()
 
