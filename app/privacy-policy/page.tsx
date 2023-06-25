ceb6b9389d49aef968795706bea2957b3f2037f8
Philani Ncube <ncbphi001@gmail.com>
2023-06-25 13:09:50 +0200

    policy pages

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,8 +1,77 @@
 import Container from "@/components/layout/Container";
 
 const page = () => {
-  return <Container>
-    <h1 className="text-3xl md:text-4xl font-medium">Privacy Policy</h1>
-  </Container>;
+  return (
+    <Container>
+      <h1 className="text-4xl font-bold text-gray-900">Privacy Policy</h1>
+      <p className="my-4 text-md text-gray-500">
+        Ink Art respects your privacy. This Privacy Policy describes how we
+        collect, use, and share your personal information when you visit or make
+        a purchase from our website.
+      </p>
+      <h3 className="text-lg text-gray-500">
+        What personal information do we collect?
+      </h3>{" "}
+      <p className="my-4 text-md text-gray-500">
+        We collect the following personal information from you when you visit
+        our website:
+      </p>
+      <ul className="mt-2 text-md text-gray-500">
+        <li className=" text-md text-gray-500">Your name and email address</li>
+        <li className=" text-md text-gray-500">
+          Your shipping and billing address
+        </li>
+        <li className=" text-md text-gray-500">Your payment information</li>
+        <li className=" text-md text-gray-500">Your IP address</li>
+        <li className=" text-md text-gray-500">Your browsing activity</li>
+      </ul>{" "}
+      <p className="my-4 text-md text-gray-500">
+        We also collect information about your interactions with our website,
+        such as the pages you visit, the products you view, and the searches you
+        conduct.
+      </p>{" "}
+      <h3 className="text-lg text-gray-500">
+        How do we use your personal information?
+      </h3>{" "}
+      <p className="my-4 text-md text-gray-500">
+        We use your personal information to:
+      </p>{" "}
+      <ul className="mt-2 text-md text-gray-500">
+        <li className=" text-md text-gray-500">
+          Process your orders and fulfill your requests
+        </li>
+        <li className=" text-md text-gray-500">
+          Communicate with you about your orders, products, and services
+        </li>
+        <li className=" text-md text-gray-500">
+          Provide you with customer support
+        </li>
+        <li className=" text-md text-gray-500">
+          Personalize your experience on our website
+        </li>
+        <li className=" text-md text-gray-500">
+          Send you marketing and promotional materials
+        </li>
+        <li className=" text-md text-gray-500">
+          Improve our website and services
+        </li>
+        <li className=" text-md text-gray-500">
+          Comply with our legal obligations
+        </li>
+      </ul>{" "}
+      <h3 className="text-lg text-gray-500">
+        Who do we share your personal information with?
+      </h3>{" "}
+      <p className="my-4 text-md text-gray-500">
+        We may share your personal information with the following third parties:
+      </p>{" "}
+      <ul className="mt-2 text-md text-gray-500">
+        <li className=" text-md text-gray-500">Our payment processors</li>
+        <li className=" text-md text-gray-500">Our shipping partners</li>
+        <li className=" text-md text-gray-500">Our marketing partners</li>
+        <li className=" text-md text-gray-500">Our analytics providers</li>
+      </ul>{" "}
+    </Container>
+  );
 };
 export default page;
--- app/terms-and-conditions/page.tsx
+++ app/terms-and-conditions/page.tsx
@@ -1,4 +1,87 @@
+import Container from "@/components/layout/Container";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <Container>
+      <div className="w-full">
+        <h1 className="text-4xl font-bold text-gray-900">
+          Terms and Conditions
+        </h1>
+        <h3 className="text-lg text-gray-500">Welcome to Ink Art!</h3>
+        <p className="my-4 text-md text-gray-500">
+          These terms and conditions (the &apos;Terms&apos;) govern your use of
+          the Ink Art website (the &apos;Website&apos;) and any products or
+          services offered on the Website (the &apos;Services&apos;). By using
+          the Website or Services, you agree to be bound by these Terms.
+        </p>
+        <h3 className="text-lg text-gray-500">1. Intellectual Property</h3>
+        <p className="my-4 text-md text-gray-500">
+          All content on the Website, including but not limited to text, images,
+          graphics, logos, button icons, software, music, sound, and videos
+          (collectively, the &apos;Content&apos;), is the property of Ink Art or
+          its licensors and is protected by copyright, trademark, and other
+          intellectual property laws. You may not use the Content without the
+          express written permission of Ink Art.
+        </p>
+        <h3 className="text-lg text-gray-500">2. User Content</h3>
+        <p className="my-4 text-md text-gray-500">
+          If you submit any content to the Website, such as reviews, comments,
+          or images (collectively, &apos;User Content&apos;), you grant Ink Art
+          a non-exclusive, royalty-free, perpetual, irrevocable, and
+          sublicensable right to use, reproduce, modify, adapt, publish,
+          translate, create derivative works from, distribute, and display such
+          User Content in any media or format, now known or hereafter devised.
+          You represent and warrant that you own or have the necessary rights to
+          submit User Content to the Website and that such User Content does not
+          violate the intellectual property or other rights of any third party.
+        </p>
+        <h3 className="text-lg text-gray-500">3. Purchases</h3>
+        <p className="my-4 text-md text-gray-500">
+          If you purchase any products or services from the Website, you agree
+          to the terms and conditions of the applicable purchase agreement.
+        </p>
+        <h3 className="text-lg text-gray-500">4. Shipping</h3>
+        <p className="my-4 text-md text-gray-500">
+          All shipping costs are included in the final price of the order. Ink
+          Art will ship products to the address provided by the customer.
+        </p>
+        <h3 className="text-lg text-gray-500">5. Returns</h3>
+        <p className="my-4 text-md text-gray-500">
+          Ink Art offers a 30-day return policy for all products. To return a
+          product, please contact Ink Art customer service.
+        </p>
+        <h3 className="text-lg text-gray-500">6. Liability</h3>
+        <p className="my-4 text-md text-gray-500">
+          Ink Art is not liable for any damages or losses, whether direct,
+          indirect, incidental, consequential, or punitive, arising out of or in
+          connection with your use of the Website or Services.
+        </p>
+        <h3 className="text-lg text-gray-500">7. Governing Law</h3>
+        <p className="my-4 text-md text-gray-500">
+          These Terms are governed by and construed in accordance with the laws
+          of the State of California. Any disputes arising out of or in
+          connection with these Terms shall be resolved by binding arbitration
+          in accordance with the rules of the American Arbitration Association.
+        </p>
+        <h3 className="text-lg text-gray-500">8. Entire Agreement</h3>
+        <p className="my-4 text-md text-gray-500">
+          These Terms constitute the entire agreement between you and Ink Art
+          regarding your use of the Website and Services. These Terms supersede
+          all prior or contemporaneous communications, representations, or
+          agreements, whether oral or written.
+        </p>
+        <h3 className="text-lg text-gray-500">9. Changes to the Terms</h3>
+        <p className="my-4 text-md text-gray-500">
+          Ink Art may modify these Terms at any time. The most current version
+          of the Terms will be posted on the Website.
+        </p>
+        <h3 className="text-lg text-gray-500">10. Contact Us</h3>
+        <p className="my-4 text-md text-gray-500">
+          If you have any questions about these Terms, please contact Ink Art
+          customer service at info@inkart.store.
+        </p>
+      </div>
+    </Container>
+  );
 };
 export default page;
