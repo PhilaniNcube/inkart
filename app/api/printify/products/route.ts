b0fec972e7adfe8f5f374bfcaf3a893ffca39d08
Philani Ncube <ncbphi001@gmail.com>
2023-07-08 19:15:50 +0200

    match req and response headers

--- app/api/printify/products/route.ts
+++ app/api/printify/products/route.ts
@@ -37,6 +37,9 @@ export async function POST(request: Request) {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${process.env.NEXT_PUBLIC_PRINTIFY_API_TOKEN}`,
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
     },
     body: JSON.stringify({
       title:title,
@@ -103,9 +106,11 @@ export async function POST(request: Request) {
   }, {
     status: 200,
     headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.NEXT_PUBLIC_PRINTIFY_API_TOKEN}`,
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
     }
   })
 }
@@ -118,9 +123,11 @@ export async function POST(request: Request) {
   }, {
     status: 200,
     headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.NEXT_PUBLIC_PRINTIFY_API_TOKEN}`,
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
     }
   })
 
