ea59b5d860ae0e96718a8fd2a234ce279758f99f
Philani Ncube <ncbphi001@gmail.com>
2023-07-08 19:10:04 +0200

    request headers

--- app/api/printify/products/route.ts
+++ app/api/printify/products/route.ts
@@ -37,9 +37,6 @@ export async function POST(request: Request) {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${process.env.NEXT_PUBLIC_PRINTIFY_API_TOKEN}`,
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
     },
     body: JSON.stringify({
       title:title,
